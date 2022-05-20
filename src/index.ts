21d44868a3ad08e0250a90abc4930ac9d26fd16a
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-05-20 23:38:10 +0300

    fix exports types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { Client } from "./classes/Client";
+export { Identifier } from "./types/Identifier";
+export { Payload } from "./types/Payload";
+export { Request } from "./types/Request";
+export { Response, ResponseError, ResponseEvent } from "./types/Response";
